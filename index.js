6de3bb85f9fdff70785ef831d0e9d9e63c720f9a
yboCrafts <ytimola123@gmail.com>
2022-09-02 18:29:14 +0800

    Day 1 (afternoon) - Homework

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const button = document.querySelector('button.greeting');
+const heading = document.querySelector('h1#_second_');
 
 function changeText(ev){
+    const textInput = document.querySelector('#input-text');
     ev.target.textContent = 'Clickity click'
+    heading.textContent = textInput.value;
 }
 
 button.addEventListener('click', changeText);
