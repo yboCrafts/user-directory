35677212a60671c7209ca16f53f1bf8c268007b6
yboCrafts <ytimola123@gmail.com>
2022-09-03 17:51:51 +0800

    Add multiple user names

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const form = document.querySelector('#userForm');
 
 const handleSubmit = function(ev) {
     ev.preventDefault();
-    const heading = document.querySelector('h1');
+    const users = document.querySelector('#users');
+    const newHeading = document.createElement('h1');
     const f = ev.target;
     const userName = f.userName.value;
-    heading.textContent = userName;
+    
+    newHeading.textContent = userName;
+
+    users.appendChild(newHeading);
+
+    f.userName.value = '';
 }
 
 form.addEventListener('submit', handleSubmit);
