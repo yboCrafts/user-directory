912015ec5a2c2446538c54832b23fbdbb1abc678
yboCrafts <ytimola123@gmail.com>
2022-09-03 19:20:44 +0800

    Display users in their favorite colors

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const form = document.querySelector('#userForm');
 
-const handleSubmit = function(ev) {    
+const handleSubmit = function(ev) {     
     ev.preventDefault();
     const users = document.querySelector('#users');    
     const f = ev.target;
     const userName = f.userName.value;
     const age = f.age.value;
+    const favoriteColor = f.favoriteColor.value;
 
     const p = document.createElement('p');
     p.textContent = `${userName}, ${age}`;
-    
+
     users.appendChild(p);
+    
+    p.style.backgroundColor = favoriteColor;
 
     f.reset();
     f.userName.focus();
