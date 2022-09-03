4148ac9a8a20dd07f0e7ea1b8813309e5eec7419
yboCrafts <ytimola123@gmail.com>
2022-09-03 18:26:14 +0800

    Add age field

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('#userForm');
 
-const handleSubmit = function(ev) {
+const handleSubmit = function(ev) {    
     ev.preventDefault();
     const users = document.querySelector('#users');    
     const f = ev.target;
     const userName = f.userName.value;
-    users.innerHTML += '<p>' + userName + '</p>';
+    const age = f.age.value;
+    users.innerHTML += '<p>' + userName + ', ' + age + '</p>';
 
     f.userName.value = '';
 }
