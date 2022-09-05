025d91780d0bab6b21e8155c736d7ffb0190e451
yboCrafts <ytimola123@gmail.com>
2022-09-05 14:00:33 +0800

    Build list items with either a string or an element

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
-const form = document.querySelector('#userForm');
+const form = document.querySelector('#userForm')
 
-const renderColor = function() {
-    const favoriteColor = document.querySelector('#favoriteColor').value;
-    const colorDiv = document.createElement('div');   
-    colorDiv.style.width = '6rem';
-    colorDiv.style.height = '3rem';
-    colorDiv.style.backgroundColor = favoriteColor;
-    
-    return colorDiv;
+function renderColor(color) {
+    const colorDiv = document.createElement('div')  
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+    colorDiv.style.backgroundColor = color
+    return colorDiv
 }
 
-const renderListItem = function(itemName, itemValue) {
-    const listItem = document.createElement('li');
-    listItem.textContent = `${itemName + ':'} ${itemValue}`;
-
-    return listItem;
+function renderListItem(label, value) {
+    const item = document.createElement('li')
+    item.textContent = `${label}: `
+    try {
+        item.appendChild(value)
+    } catch(e) {
+        item.textContent += value
+    }
+    return item
 }
 
-const renderList = function() {
-    const userName = document.querySelector('#userName').value;
-    const age = document.querySelector('#age').value;
 
-    const nameItem = renderListItem('Name', userName);
-    const ageItem = renderListItem('Age', age);
-    const colorItem = renderListItem('Favorite Color', '');  
-    colorItem.appendChild(renderColor());
-
-    const list = document.createElement('ul');
-    list.appendChild(nameItem);
-    list.appendChild(ageItem);
-    list.appendChild(colorItem);
-
-    return list;
-}
 
 const handleSubmit = function(ev) {        
-    ev.preventDefault();        
-    const f = ev.target;    
+    ev.preventDefault()        
+    const f = ev.target 
+    const userName = f.userName.value
+    const age = f.age.value
+    const favoriteColor = f.favoriteColor.value   
 
-    const users = document.querySelector('#users');
-    users.appendChild(renderList());        
+    const list = document.createElement('ul')
+    list.appendChild(renderListItem('Name', userName))
+    list.appendChild(renderListItem('Age', age))
+    list.appendChild(renderListItem('Favorite Color', renderColor(favoriteColor)))
 
-    f.reset();
-    f.userName.focus();
+    const users = document.querySelector('#users')
+    users.appendChild(list)       
+
+    f.reset()
+    f.userName.focus()
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit)
